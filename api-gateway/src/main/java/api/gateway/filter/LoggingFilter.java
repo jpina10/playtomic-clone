c65c13c43a22ed2e78d8f167e585624911552192
Pina, Joao <joao.pina@sky.uk>
2024-08-26 15:08:47 +0100

    refactor log message to include more information about the request

--- api-gateway/src/main/java/api/gateway/filter/LoggingFilter.java
+++ api-gateway/src/main/java/api/gateway/filter/LoggingFilter.java
@@ -1,13 +1,17 @@
 package api.gateway.filter;
 
+import api.gateway.model.RequestInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -16,7 +20,7 @@ import java.util.Optional;
 public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Config> {
 
     private static final String GATEWAY_REQUEST_URL = "org.springframework.cloud.gateway.support.ServerWebExchangeUtils.gatewayRequestUrl";
-    private static final String RECEIVED_CALL = "Received a call to the gateway for: {}";
+    private static final String RECEIVED_CALL = "Received a {} request to the gateway for: {} with headers: {} and parameters: {}";
     private static final String FORWARDED_CALL = "Forwarded call to service: {} with uri: {}";
     private static final String NO_ROUTE = "No route found for the request.";
 
@@ -28,7 +32,9 @@ public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Co
     @Override
     public GatewayFilter apply(Config config) {
         return (exchange, chain) -> {
-            log.info(RECEIVED_CALL, exchange.getRequest().getURI());
+            RequestInfo requestInfo = getRequestInfo(exchange);
+
+            log.info(RECEIVED_CALL, requestInfo.getMethod(), exchange.getRequest().getURI(), requestInfo.getHeaders(), requestInfo.getParameters());
 
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
                 String forwardedUri = Objects.requireNonNull(exchange.getAttribute(GATEWAY_REQUEST_URL)).toString();
@@ -44,6 +50,18 @@ public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Co
                         );
             }));
         };
+    }
+
+    private RequestInfo getRequestInfo(ServerWebExchange exchange) {
+        String method = exchange.getRequest().getMethod().name();
+
+        Map<String, String> headers = new HashMap<>();
+        exchange.getRequest().getHeaders().forEach((key, values) -> headers.put(key, String.join(",", values)));
+
+        Map<String, String> parameters = new HashMap<>();
+        exchange.getRequest().getQueryParams().forEach((key, values) -> parameters.put(key, String.join(",", values)));
+
+        return new RequestInfo(method, headers, parameters);
     }
 
     public static class Config {
