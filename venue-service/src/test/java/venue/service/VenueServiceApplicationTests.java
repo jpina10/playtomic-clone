44ff6684a2abe02142e968b4bed73a42c42c9d1f
Pina, Joao <joao.pina@sky.uk>
2024-09-17 12:22:45 +0100

    add venue service

---
+++ venue-service/src/test/java/venue/service/VenueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package venue.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenueServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
